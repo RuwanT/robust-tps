babed00c0a56f2e652b7d9b19fcd7f3e7da4f12e
RuwanT <ruwant.email@gmail.com>
2019-05-17 14:11:18 +1000

    Update main.m

--- main.m
+++ main.m
@@ -4,9 +4,8 @@ rng(2);
 
 n = 1000; % Number of data points
 
-% generate data (select one of three below)
+% generate data (select one of two below)
 [x, y, x_true, y_true, sp] = get_input_data(n, 10, .1, 'simple_sin_struct');
-% [x, y, x_true, y_true, sp] = get_input_data(n, 10, .1, 'simple_sin_nstruct');
 % [x, y, x_true, y_true, sp] = get_input_data(n, 4, .1, 'complex_sin_struct');
 
 % Run robust TPS algorithm
