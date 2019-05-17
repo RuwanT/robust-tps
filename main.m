384c9d6aa7543f7bb5db45f1e1d59a5a40f2c01f
RuwanT <ruwant.email@gmail.com>
2019-05-17 13:22:08 +1000

    Update main.m

--- main.m
+++ main.m
@@ -4,9 +4,9 @@ clear all
 n = 1000; % Number of data points
 
 % generate data (select one of three below)
-% [x, y, x_true, y_true, sp] = get_input_data(n, 10, .1, 'simple_sin_struct');
+[x, y, x_true, y_true, sp] = get_input_data(n, 10, .1, 'simple_sin_struct');
 % [x, y, x_true, y_true, sp] = get_input_data(n, 10, .1, 'simple_sin_nstruct');
-[x, y, x_true, y_true, sp] = get_input_data(n, 4, .1, 'complex_sin_struct');
+% [x, y, x_true, y_true, sp] = get_input_data(n, 4, .1, 'complex_sin_struct');
 
 % Run robust TPS algorithm
 [inlX, inlY, outX, outY] = get_robust_TPS_fit(x,y, floor(n/2),floor(n/10),floor(n/2),sp);
